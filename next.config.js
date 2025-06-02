84e1382410e6509d10f4439290f5317c1841a64b
notafrogo <110315743+notafrogo@users.noreply.github.com>
2025-06-02 08:09:57 -0700

    fix for static pages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {},
+}
 
 module.exports = withBundleAnalyzer(nextConfig)
